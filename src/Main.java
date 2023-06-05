6caf29d0b676ebf04f076cd6f680e4625704d6b3
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-05 19:02:45 +0300

    Первое задание

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {   task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();}
+    private static void task1() {
+        System.out.println("задача 1");
+        int age = 24, age2 = 16;
+        if ( age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }else {System.out.println("Если возраст человека равен "+age+", то он не достиг совершеннолетия, нужно немного подождать");}
+        if ( age2 >= 18) {
+            System.out.println("Если возраст человека равен " + age2 + ", то он совершеннолетний");
+        }else {System.out.println("Если возраст человека равен "+age2+", то он не достиг совершеннолетия, нужно немного подождать");}
+    }
+    private static void task2() {
+        System.out.println("задача 2");
+
+    }
+    private static void task3() {
+        System.out.println("задача 3");}
+    private static void task4() {
+        System.out.println("задача 4");}
+
+    private static void task5() {
+        System.out.println("задача 5");}
+
+    private static void task6() {
+        System.out.println("задача 6");}
+
+    private static void task7() {
+        System.out.println("задача 7");}
+
+    private static void task8() {
+        System.out.println("задача 8");}
+}
